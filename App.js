47fe6f87ce27024da65778a06dfa61008943fd54
Shane Wilson <shanewilsonpro@gmail.com>
2023-10-25 13:55:01 -0700

    tab navigator set up

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import TabNavigator from "./src/navigators/tab-navigator";
+import MovieDetailsScreen from "./src/screens/movie-details-screen";
+import SeatBookingScreen from "./src/screens/seat-booking-screen";
 
-import HomeScreen from "./src/screens/home-screen";
+const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <SafeAreaView>
-      <HomeScreen />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen
+          name="Tab"
+          component={TabNavigator}
+          options={{ animation: "default" }}
+        />
+        <Stack.Screen
+          name="MovieDetails"
+          component={MovieDetailsScreen}
+          options={{ animation: "slide_from_right" }}
+        />
+        <Stack.Screen
+          name="SeatBooking"
+          component={SeatBookingScreen}
+          options={{ animation: "slide_from_bottom" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/navigators/styles/tab-navigator.styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+import { COLORS, SPACING } from "../../theme/theme";
+
+export const styles = StyleSheet.create({
+  activeTabBackground: {
+    backgroundColor: COLORS.Black,
+    padding: SPACING.space_18,
+    borderRadius: SPACING.space_18 * 10,
+  },
+});
